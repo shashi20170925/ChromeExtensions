61d26c1f65c9c147cb0d259a871caad304e9f172
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2017-10-03 21:09:49 -0500

    fixed

--- LKM Viewer/content.js
+++ LKM Viewer/content.js
@@ -10,29 +10,46 @@ function FindArrayElementValueByID(id) {
     });
     return tempText1;
 }
-
+var wasInLKMMode = true;
+var isFirstClick = true;
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.todo == "LKMMode") {
-        $(".portalapp").find("span[id*='LKMKEY_']").each(function () {
-            var lkmKey = $(this)[0].id;
-            var lkmText = $(this).text();
-            var lkmObject = {};
-            lkmObject[lkmKey] = lkmText;
-            lkmArray.push(lkmObject);
-            $(this).attr('style', 'background-color:yellow');
-            //$(this).css("background-color:blue;");
-            $(this).html($(this)[0].id.replace("LKMKEY_", ""));
-        });
+        if (wasInLKMMode) {
+            $(".portalapp").find("span[id*='LKMKEY_']").each(function () {
+                var lkmKey = $(this)[0].id;
+                var lkmText = $(this).text();
+                var lkmObject = {};
+                lkmObject[lkmKey] = lkmText;
+                lkmArray.push(lkmObject);
+                $(this).attr('style', 'background-color:yellow');
+
+                //$(this).css("background-color:blue;");
+                $(this).html($(this)[0].id.replace("LKMKEY_", ""));
+            });
+            wasInLKMMode = false;
+            isFirstClick = false;
+        }
+
+      
         //  sendResponse(returnTable);
     }
     else if (request.todo == "NormalMode") {
-        $(".portalapp").find("span[id*='LKMKEY_']").each(function () {
-            var tempText = FindArrayElementValueByID($(this)[0].id);
-            if (tempText != '' && tempText != undefined) {
-                $(this).attr('style', '');
-                $(this).html(tempText);
-            }
-        });
+
+        if (!isFirstClick) {
+
+            wasInLKMMode = true;
+            $(".portalapp").find("span[id*='LKMKEY_']").each(function () {
+                var tempText = FindArrayElementValueByID($(this)[0].id);
+                if (tempText != '' && tempText != undefined) {
+                    $(this).attr('style', '');
+                    $(this).html(tempText);
+                }
+            });
+        }
+        else {
+            alert("Please toggle into LKM mode before clicking on Normal Mode");
+        }
+
         //   sendResponse(returnTable);
     }
 });
